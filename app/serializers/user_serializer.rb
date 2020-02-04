62bd6da464756799e88ce04391b433cfe4c59fc7
Reina <r.clarkemitchell@gmail.com>
2020-02-04 17:14:22 -0500

    added method to user serializer for candidate info

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,21 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :email, :admin, :candidate_id
+  attributes :id, :first_name, :last_name, :username, :email, :admin, :candidate_id, :candidate_info
+
+  def candidate_info
+    {first_name: Candidate.find(self.object.candidate.id).first_name,
+    last_name: Candidate.find(self.object.candidate.id).last_name,
+    age: Candidate.find(self.object.candidate.id).age,
+    political_party_identification: Voter.find(self.object.candidate.id).political_party_identification,
+    street_number: Candidate.find(self.object.candidate.id).street_number,
+    street_name: Candidate.find(self.object.candidate.id).street_name,
+    city: Candidate.find(self.object.candidate.id).city,
+    state: Candidate.find(self.object.candidate.id).state,
+    zip_code: Candidate.find(self.object.candidate.id).zip_code}
+end
+
+
+
+
 end
 
 
